fd6d5c3267b44cc024fd70a99508365587f3fc6a
oreanroy <oreanroy@gmail.com>
2018-12-27 22:32:19 +0530

    login validation using jquery added not working.

---
+++ public/js/myJs/login.js
@@ -0,0 +1,36 @@
+$("#modalForm").validate({
+    rules: {
+        nameModal: {
+            required: true,
+            minlength: 8
+        },
+        emailModal: {
+            required: true,
+            minlength: 5
+        },
+        numberModal: {
+            required: true,
+            minlength: 12
+        },
+    },
+    messages: {
+        nameModal: {
+            required: "Please enter name",
+            minlength: "Your data must be at least 8 characters"
+        },
+        emailModal: {
+            required: "Please enter email",
+            minlength: "Your data must be at least 5 characters"
+        },
+        numberModal: {
+            required: "Please enter number",
+            minlength: "Number should be atleast 9 character"
+        },
+    }
+});
+
+
+
+
+
+  
